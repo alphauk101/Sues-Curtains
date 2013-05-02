1f4a1db427f6bd323b2c678ceb6ec8af7d40bc9a
alphauk101 <alphauk101@hotmail.com>
2013-05-02 12:27:54 +0100

    new icon

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
         <!-- Add your site or application content here -->
         <div id="wrapper">
             <div id="header">
-                <div id="logo"><img src="images/logo.png" alt="Sues Curtains"/><div id="tele"><img src="images/tele.png" alt="Telephone: 012345687886"/></div></div>
+                <div id="logo"><img src="images/logo.png" alt="Sues Curtains"/><div id="tele"><img src="images/tele.png" alt="Telephone: 012345687886"/><img src="images/fb_icon.png" alt="Facebook"/></div></div>
             </div>
             <nav class="gradient">This is nav</nav>
             
